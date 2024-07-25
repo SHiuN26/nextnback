3f88d86f1160474de1eff47e47ce9a46beb33e5a
DESKTOP-UKI6G6Q\SHiuN <justodme26@gmail.com>
2024-07-25 13:55:27 +0800

    add MUI

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogPost: React.FC = () => {
+  return (
+    <div>
+      <h1>Blog </h1>
+      <p>This is the content of blog post</p>
+    </div>
+  );
+};
+
+export default BlogPost;
